521502ed99afb33704c2de39f04f98a62f2a5b53
Vincent Lin <soulace2425@outlook.com>
2022-10-14 10:58:56 -0700

    Fix compression, attempt 3

--- compress.sh
+++ compress.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/bash
 # TO BE RUN AT THE REPOSITORY ROOT
 
-tar -cvf test_shuf.tgz src/test_shuf.py src/test_cases src/test_input
+cd src
+tar -cvf ../test_shuf.tgz test_shuf.py test_cases test_input
+cd ..
